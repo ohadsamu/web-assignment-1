af7996a310c4e136db45106e9c26d74e8d188728
idoRevah <47219346+idoRevah@users.noreply.github.com>
2025-01-20 22:33:57 +0200

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   console.log(process.env.MONGO_URI);
   try {
+    console.log("Mongo is trying to connect")
     await mongoose.connect(process.env.MONGO_URI || "", {
       family: 4,
     });
-    console.log("MongoDB connected...");
+    console.log("MongoDB connected!");
   } catch (err) {
     console.error("MongoDB connection error:", err);
     process.exit(1);
