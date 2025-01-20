2cfdbdef16f94df9e5ce122f687d2e86feb02962
idoRevah <47219346+idoRevah@users.noreply.github.com>
2025-01-20 22:35:39 +0200

    Update commentController.ts

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -6,13 +6,6 @@ export const getComments = async (req: any, res: any) => {
   res.json(comments);
 };
 
-export const getCommentById = async (req: any, res: any) => {
-  const comment = await Comment.findById(req.params.id);
-  if (!comment) return res.status(404).json({ message: "Comment not found" });
-
-  res.json(comment);
-};
-
 export const createComment = async (req: any, res: any) => {
   const { content, post } = req.body;
   const comment = new Comment({
@@ -22,6 +15,22 @@ export const createComment = async (req: any, res: any) => {
   await comment.save();
   res.status(201).json(comment);
 };
+
+export const getCommentById = async (req: any, res: any) => {
+  const comment = await Comment.findById(req.params.id);
+  if (!comment) return res.status(404).json({ message: "Comment not found" });
+
+  res.json(comment);
+};
+
+
+export const deleteComment = async (req: any, res: any) => {
+  const comment = await Comment.findById(req.params.id);
+  if (!comment) return res.status(404).json({ message: "Comment not found" });
+  await Comment.deleteOne({ _id: req.params.id });
+  res.json({ message: "Comment Deleted" });
+};
+
 
 export const updateComment = async (req: any, res: any) => {
   const comment = await Comment.findById(req.params.id);
@@ -33,9 +42,3 @@ export const updateComment = async (req: any, res: any) => {
   res.json(comment);
 };
 
-export const deleteComment = async (req: any, res: any) => {
-  const comment = await Comment.findById(req.params.id);
-  if (!comment) return res.status(404).json({ message: "Comment not found" });
-  await Comment.deleteOne({ _id: req.params.id });
-  res.json({ message: "Comment deleted" });
-};
